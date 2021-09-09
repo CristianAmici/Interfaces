5c30d434545e964abd72927d3703fec748311dab
Cristian Amici <cristianamici@gmail.com>
2021-09-08 23:03:50 -0300

    probando filtros, agregados el gris y sepia

---
+++ src/js/js.js
@@ -0,0 +1,74 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", ()=>{
+
+        var c = document.getElementById("MyCanvas");
+        var ctx = c.getContext("2d");
+
+        var imageData = ctx.createImageData(c.height, c.width);
+        let imageScaledWidth = c.width;
+        let imageScaledHeight = c.height;
+        var imagen = new Image();
+        imagen.src = "src/images/messirve.jpg";
+        imagen.onload = loadImage()
+        let buttonOriginal= document.getElementById("original").addEventListener("click", loadImage);
+        function loadImage() {
+                ctx.drawImage(imagen, 0, 0);
+        }
+        let buttongris = document.getElementById("gris");
+        buttongris.addEventListener("click", function aplicarFiltroGrises(){
+                let r;
+                let b;
+                let g;
+                let imageData = ctx.getImageData(0, 0, c.width, c.height);
+                for (let y = 0; y < imageData.height; y++) {
+                        for (let x = 0; x < imageData.width; x++) {
+                                let index = (x + y * imageData.width) * 4;
+                                r = getRed(imageData, x, y);
+                                g = getGreen(imageData, x, y);
+                                b = getBlue(imageData, x, y);
+                                let grey = (r + g + b) / 3;
+                                imageData.data[index + 0] = grey;
+                                imageData.data[index + 1] = grey;
+                                imageData.data[index + 2] = grey;
+                        }
+                }
+                ctx.putImageData(imageData, 0, 0);
+        });
+        let buttonsepia = document.getElementById("sepia");
+        buttonsepia.addEventListener("click", function aplicarFiltroSepia(){
+                let r;
+                let b;
+                let g;
+                let imageData = ctx.getImageData(0, 0, c.width, c.height);
+                for (let y = 0; y < imageData.height; y++) {
+                        for (let x = 0; x < imageData.width; x++) {
+                                let index = (x + y * imageData.width) * 4;
+                                r = getRed(imageData, x, y);
+                                g = getGreen(imageData, x, y);
+                                b = getBlue(imageData, x, y);
+                                imageData.data[index + 0] = Math.trunc(0.4 * r + 0.8 * g + 0.2 * b);
+                                imageData.data[index + 1] = Math.trunc(0.35 * r + 0.7 * g + 0.175 * b);
+                                imageData.data[index + 2] = Math.trunc(0.25 * r + 0.5 * g + 0.125 * b);;
+                        }
+                }
+                ctx.putImageData(imageData, 0, 0);
+        });
+
+        //Obtiene la cantidad de rojo del pixel
+        function getRed(imageData, x, y) {
+                let index = (x + y * imageData.width) * 4;
+                return imageData.data[index + 0];
+        }
+
+        //Obtiene la cantidad de verde del pixel
+        function getGreen(imageData, x, y) {
+                let index = (x + y * imageData.width) * 4;
+                return imageData.data[index + 1];
+        }
+
+        //Obtiene la cantidad de azul del pixel
+        function getBlue(imageData, x, y) {
+                let index = (x + y * imageData.width) * 4;
+                return imageData.data[index + 2];
+        }
+})
