64ccdab27d625fe4d1028f2716e81c2c6fa1131b
Cristian Amici <cristianamici@gmail.com>
2021-10-10 23:18:10 -0300

    agregamos comentarios

--- Entregable2/src/js/main.js
+++ Entregable2/src/js/main.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     var img2 = new Image();
     var img3 = new Image();
     var img4 = new Image();
-    
+
     //
     var coorY, coorX;
     var turno = 2;
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     var matrix = new Array();
     var imgJ1 = document.getElementById('j1');
     var imgJ2 = document.getElementById('j2');
-    var imgGanador= document.getElementById("jGanador");
+    var imgGanador = document.getElementById("jGanador");
     const valoresOriginales = new Array();
     var ganador = document.getElementById("ganador");
     var mensaje = document.getElementById("mensajeGanador");
@@ -97,10 +97,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
             segundos = 30
-            if(turno==2){
-                turno=1;
-            }else{
-                turno=2;
+            if (turno == 2) {
+                turno = 1;
+            } else {
+                turno = 2;
             }
         }
     }
@@ -109,6 +109,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.getElementById("nuevoJuego").addEventListener("click", jugar)
 
+    //permite reiniciar el canvas, el cronometro y el juego
     function reiniciar() {
         clearInterval(intervalo)
 
@@ -133,7 +134,7 @@ document.addEventListener("DOMContentLoaded", () => {
         valoresOriginales.splice(0, valoresOriginales.length)
         juego.fichas.splice(0, juego.fichas.length)
     }
-
+    //Se encarga de cargar el tablero y las fichas con las imagenes que seleccione el usuario, ademas del timer
     function jugar() {
         let tipoJuego = document.getElementById("selectJuego").value;
         valorDeLinea = parseInt(tipoJuego);
@@ -172,9 +173,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 matrix[x] = lugares;
                 coorX = x * 100;
             }
-          
+
         }
-        
+
         //determinamos cuales van a ser las fichas que se van a jugar y tambien las mostramos en la pagina
         rivales = document.getElementById("selectRivales").value;
         if (rivales == "aves") {
@@ -198,6 +199,7 @@ document.addEventListener("DOMContentLoaded", () => {
             img3.src = "src/css/images/fichaEscorpion.png";
             imgJ2.src = img3.src;
         }
+        //cargamos las fichas del jugador 1
         img2.onload = function () {
             for (let x = 8 + valorDeLinea; x < 11 + valorDeLinea; x++) {
                 for (let y = 0; y < 25 + valorDeLinea; y++) {
@@ -217,7 +219,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 coorX = x * 100;
             }
         }
-
+        //cargamos las fichas del jugador 2
         img3.onload = function () {
             for (let x = 11 + valorDeLinea; x < 14 + valorDeLinea; x++) {
                 for (let y = 0; y < 25 + valorDeLinea; y++) {
@@ -251,7 +253,7 @@ document.addEventListener("DOMContentLoaded", () => {
         ctx.fillRect(space.x, space.y, FICHA_WIDTH, FICHA_HEIGHT);
         ctx.strokeRect(space.x, space.y, FICHA_WIDTH, FICHA_HEIGHT);
     }
-
+    //Por cada espacio en juego.spaces se dibuja donde se deposita las fichas
     function drawSpaces() {
         let tope = 0;
         juego.spaces.forEach(function (space) {
@@ -266,7 +268,8 @@ document.addEventListener("DOMContentLoaded", () => {
     function dibujarFicha(ficha, ctx) {
         ctx.drawImage(ficha.img, ficha.x, ficha.y);
     }
-
+    //Limpiamos el canvas donde se dibujan las fichas, y volvemos a dibujar las fichas, salvo la que el usuario
+    //esta soteniedno y aquelas que estan en el tablero   
     function drawCards() {
         context.cards.clearRect(
             0, 0,
@@ -280,6 +283,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
         });
     }
+    //Mediante el evento mousedown chequeamos que el lugar donde se genero el mousedown sea el mismo lugar
+    //donde se encuentre alguna de las fichas.  Si esto tambien se cumple la ficha se borra de el canvas donde estan las demas fichas
+    //(cards) y la dibuja en el canvas drag.
     canvases.drag.addEventListener("mousedown", function (e) {
         var ficha;
         turnoTitulo.innerHTML = ""
@@ -293,8 +299,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (turno != juego.fichas[index].jugador) {
                     juego.holdingCard = ficha;
                     juego.cursorOffset = {
-                        x: e.clientX - ficha.x,
-                        y: e.clientY - ficha.y
+                        x: e.clientX- ficha.x,
+                        y: e.clientY- ficha.y
                     };
 
                     drawCards();
@@ -315,7 +321,9 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
     });
-
+     //Mediante el evento up, que es cuando el usuario levanta el click, chequea que el lugar donde soltas la ficha sea
+    //el mismo lugar donde se deposita la ficha para que caiga. De ser asi y que la columna no este llena la ficha se cae.
+    //De no ser asi, la ficha vuelve a su lugar.
     canvases.drag.addEventListener("mouseup", function () {
         juego.isMouseDown = false;
         var fichaPorCaer;
@@ -326,33 +334,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
             juego.cursorOffset = null;
 
-          
 
-                for (var index = 0; index < juego.spaces.length; index++) {
-                    var s = juego.spaces[index];
-    
-                    if (Math.abs(ficha.x - s.x) < (FICHA_WIDTH / 1.75) // si el 40% de la figura esta sobre el espacio se deposita
-                        && Math.abs(ficha.y - s.y) < (FICHA_HEIGHT / 1.75)
-                    ) {
-                       ficha.x = s.x;
-                       ficha.y = s.y;
-                       matrix[s.x/100+1].forEach(element => {
-                          if(element.jugador==0){
 
-                              didMatch= true;
-                              turno = ficha.jugador;
-                              juego.holdingCard = null;
-                        }
-                      });
-                       /*  if(matrix[s.x/100+1][0].jugador!=0){    //si la linea esta llena no se deposita la ficha
-                            didMatch= true;
+            for (var index = 0; index < juego.spaces.length; index++) {
+                var s = juego.spaces[index];
+
+                if (Math.abs(ficha.x - s.x) < (FICHA_WIDTH / 1.75) // si el 40% de la figura esta sobre el espacio se deposita
+                    && Math.abs(ficha.y - s.y) < (FICHA_HEIGHT / 1.75)
+                ) {
+                    ficha.x = s.x;
+                    ficha.y = s.y;
+                    matrix[s.x / 100 + 1].forEach(element => { //si la linea esta llena no se deposita la ficha
+                        if (element.jugador == 0) {
+
+                            didMatch = true;
                             turno = ficha.jugador;
                             juego.holdingCard = null;
-                            break;
-                        } */
-
-                    }
+                        }
+                    });
                 }
+            }
         }
 
 
@@ -392,7 +393,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
     });
-
+    //dibujamos la ficha si es seleccionada sobre el canvas drag y lo quitamos del canvas de fichas
     canvases.drag.addEventListener("mousemove", function (e) {
         if (juego.cursorOffset && juego.holdingCard != null) {
             var ficha = juego.holdingCard;
@@ -441,7 +442,7 @@ document.addEventListener("DOMContentLoaded", () => {
         var cordenadaAnteriorY;
         var espacioenX = fichaPorCaer.x / 100 + 1;
         var hastaDondeTienequeBajar;
-        for (let y = 0; y < (6+valorDeLinea); y++) {
+        for (let y = 0; y < (6 + valorDeLinea); y++) {
             if (matrix[espacioenX][y].jugador == 0) {
                 hastaDondeTienequeBajar = y;
             }
@@ -469,41 +470,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     }
-
+    //mediante funciones recursivas vamos controlando cada vez que se deposita una ficha dentro del espacio establecido
+    //que haya 4, 5,6 o 7 en linea segun el tipo de juego seleccionado por el usuario que se ve reflejado por la variable valorDeLinea
     function hayGanador(X, Y, j) {
-        if(j==1)
-        imgGanador.src= img2.src;
-        else 
-        imgGanador.src= img3.src;
-        ganador.innerHTML = "Felicitaciones jugador " + j+" has ganado!!"
+        if (j == 1)
+            imgGanador.src = img2.src;
+        else
+            imgGanador.src = img3.src;
+        ganador.innerHTML = "Felicitaciones jugador " + j + " has ganado!!"
         var linea = 1;
         var linea2 = 0;
         linea = horizontalAdelante(X, Y, j, linea);
         linea2 = horizontalAtras(X, Y, j, linea2);
-        linea+=linea2;
-        if (linea < (4+valorDeLinea)) {
+        linea += linea2;
+        if (linea < (4 + valorDeLinea)) {
             linea = 1;
             linea2 = 0;
             linea = verticalAbajo(X, Y, j, linea)
             linea2 = verticalArriba(X, Y, j, linea2)
-            linea+=linea2;
-            if (linea< (4+valorDeLinea)) {
+            linea += linea2;
+            if (linea < (4 + valorDeLinea)) {
                 linea = 1;
                 linea2 = 0;
                 linea = diagonalPuntaArribaDerecha(X, Y, j, linea)
                 linea2 = diagonalPuntaAbajoIzquierda(X, Y, j, linea2)
-                linea+=linea2;
-                if (linea < (4+valorDeLinea)) {
+                linea += linea2;
+                if (linea < (4 + valorDeLinea)) {
                     linea = 1;
                     linea2 = 0;
                     linea = diagonalPuntaArribaIzquierda(X, Y, j, linea)
                     linea2 = diagonalPuntaAbajoDerecha(X, Y, j, linea2)
-                    linea+=linea2;
+                    linea += linea2;
                 }
 
             }
         }
-        if (linea == (4+valorDeLinea)) {
+        if (linea == (4 + valorDeLinea)) {
             document.getElementById("html").classList.add("fondoGanador");
             mensaje.classList.toggle("mensajeVisible");
             document.getElementById('minutos').innerHTML = "";
@@ -511,14 +513,15 @@ document.addEventListener("DOMContentLoaded", () => {
             reiniciar();
         }
     }
-
+    // mostramos en un div superpuesto al tablero al ganador
     function mensajeGanador() {
         document.getElementById("html").classList.remove("fondoGanador");
         mensaje.classList.remove("mensajeVisible");
         mensaje.classList.add("mensajeOculto");
     }
 
-
+    // el resto de funciones son las que permiten controlar las lineas horizontales, verticales y diagonales 
+    //hacia arriba, abajo, atras y adelante desde el lugar donde fue depositada la ultima ficha
     function horizontalAdelante(X, Y, j, linea) {
         if ((X + 1) < matrix.length && Y < matrix[X].length) {
 
@@ -533,7 +536,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if ((X - 1) >= 0 && Y < matrix[X].length) {
 
             if (matrix[X - 1][Y].jugador == j && (X - 1) >= 0) {      //X-1, Y
-             
+
                 linea2 = horizontalAtras(X - 1, Y, j, linea2 + 1)
             }
         }
@@ -543,7 +546,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (X < matrix.length && (Y + 1) < matrix[X].length) {
 
             if (matrix[X][Y + 1].jugador == j && (Y + 1) <= (7 + valorDeLinea) && matrix[X][Y + 1] != null) {      //X, Y+1
-               
+
                 linea = verticalAbajo(X, Y + 1, j, linea + 1)
             }
         }
@@ -553,7 +556,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (X < matrix.length && (Y - 1) >= 0) {
 
             if (matrix[X][Y - 1].jugador == j && (Y - 1) >= 0) {         //X,Y-1
-             
+
                 linea2 = verticalArriba(X, Y - 1, j, linea2 + 1)
             }
         }
@@ -563,7 +566,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if ((X + 1) < matrix.length && (Y + 1) < matrix[X].length) {
 
             if (matrix[X + 1][Y + 1].jugador == j && (Y - 1) <= (7 + valorDeLinea) && (X + 1) < (8 + valorDeLinea)) {      //X+1, Y-1
-             
+
                 linea = diagonalPuntaArribaDerecha(X + 1, Y + 1, j, linea + 1)
             }
         }
@@ -573,7 +576,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if ((X - 1) < matrix.length && (Y - 1) >= 0) {
 
             if (matrix[X - 1][Y - 1].jugador == j && (X - 1) >= 0 && (Y - 1) >= 0) {      //X-1, Y-1
-             
+
                 linea2 = diagonalPuntaAbajoIzquierda(X - 1, Y - 1, j, linea2 + 1)
             }
         }
@@ -584,7 +587,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if ((X - 1) >= 0 && (Y + 1) < matrix[X].length) {
 
             if (matrix[X - 1][Y + 1].jugador == j && (X - 1) >= 0 && (Y + 1) <= (7 + valorDeLinea)) {      //X-1, Y+1
-              
+
                 linea = diagonalPuntaArribaIzquierda(X - 1, Y + 1, j, linea + 1)
             }
         }
@@ -593,7 +596,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function diagonalPuntaAbajoDerecha(X, Y, j, linea2) {
         if ((X + 1) < matrix.length && (Y - 1) >= 0) {
             if (matrix[X + 1][Y - 1].jugador == j && (Y + 1) <= (7 + valorDeLinea) && (X + 1) < (8 + valorDeLinea)) {      //X+1, Y+1
-              
+
                 linea2 = diagonalPuntaAbajoDerecha(X + 1, Y - 1, j, linea2 + 1)
             }
         }
