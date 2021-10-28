93a6fcd66fed5351b3cd435f7f91600f5f2857a2
camilopez <camilopez8501@gmail.com>
2021-10-28 02:42:37 -0300

    Nuevo personaje, el jugador muere en manos de los enemigos,  se detiene parallax al morir y agacharse

--- Entregable3/src/js/main.js
+++ Entregable3/src/js/main.js
@@ -5,51 +5,89 @@ document.addEventListener("DOMContentLoaded", () => {
   var keyUp = false;
   var keyDown = false;
   var cowboy = document.getElementById('cowboy');
+  var enemy = document.getElementById('enemy');
+  var enemyFlying = document.getElementById('enemyFlying');
+  var stars = document.getElementById('stars');
+  var ground = document.getElementById('ground');
+  var mountains = document.getElementById('mountains');
   var stand = false;
   var space = false
-  var jump = false;
+  var dead = false;
+
+
+
+
+
+  function checkCondition() {
+    //console.log(enemy.offsetLeft , (cowboy.offsetLeft+200)  , enemy.offsetLeft , (cowboy.offsetLeft+195));
+    if (enemy.offsetLeft <= (cowboy.offsetLeft+200)  && enemy.offsetLeft >= (cowboy.offsetLeft+190) && cowboy.offsetTop == (enemy.offsetTop - 240) //Enemigo de tierra
+    ) {
+      endGame();
+    
+    }  
+    if (enemyFlying.offsetLeft <= (cowboy.offsetLeft+100) && enemyFlying.offsetLeft >= (cowboy.offsetLeft+30) && cowboy.classList != "cowboyDown"//Enemigo de tierra
+    ) {
+      endGame();
+    } 
+    
+  }
+
+  function endGame(){
+    
+    cowboy.className = '';
+    cowboy.classList.add("cowboyDead");
+    dead= true;
+    console.log("dd");
+  }
 
   window.addEventListener("keydown", (e) => {
-
-    if (e.key == " ") {
-      space = true
-      keyDown = false;
-      keyLeft = false;
-      keyRight = false;
-      keyUp = false;
-      stand = false;
-    } else if (e.key == "ArrowDown") {
-      stand = false;
-      keyDown = true;
-      keyLeft = false;
-      keyRight = false;
-      keyUp = false;
+    if(!dead){
+      if (e.key == " ") {
+        space = true
+        keyDown = false;
+        keyLeft = false;
+        keyRight = false;
+        keyUp = false;
+        stand = false;
+      } else if (e.key == "ArrowDown") {
+        stand = false;
+        keyDown = true;
+        keyLeft = false;
+        keyRight = false;
+        keyUp = false;
+      }
+      else if (stand) {
+        cowboy.className = '';
+        cowboy.classList.add('cowboyStand')
+      }
     }
-    else if (stand) {
-      cowboy.className = '';
-      cowboy.classList.add('cowboyStand')
-    }
+    
   })
 
-  cowboy.addEventListener('animationend', () => {
+  cowboy.addEventListener('animationend', (e) => {
 
-      stand = true;
-      keyDown = false;
-      keyLeft = false;
-      keyRight = false;
-      keyUp = false;
-      space = false;
+       
+        stand = true;
+        keyDown = false;
+        keyLeft = false;
+        keyRight = false;
+        keyUp = false;
+        space = false;
+      
+  });
 
-    }
-  );
   window.addEventListener("keyup", (e) => {
 
     if (space) {
       if (cowboy.classList.name == "cowboyDown")
         cowboy.classList.remove("cowboyDown");
+       
     } else if (keyDown) {
+     
       if (cowboy.classList.name == "cowboyJump")
         cowboy.classList.remove("cowboyJump");
+        
+        
         setTimeout(()=>{
           stand = true;
           keyDown = false;
@@ -57,27 +95,56 @@ document.addEventListener("DOMContentLoaded", () => {
           keyRight = false;
           keyUp = false;
           space = false;
-    
+          empezarFondo();
         },1200)
     }
-
-
   })
 
-  let intervalId = setInterval(function () {
-    if (space) {
-      cowboy.className = '';
-      cowboy.classList.add("cowboyJump");
-    } else if (keyDown) {
-      cowboy.className = '';
-      cowboy.classList.add('cowboyDown');
 
-    } else {
-      cowboy.className = '';
-      cowboy.classList.add('cowboyStand')
-    }
+  function detenerFondo(){
+    stars.classList.remove('stars');
+    stars.classList.add('starsStatic');
+    mountains.classList.remove('mountains');
+    mountains.classList.add('mountainsStatic');
+    ground.classList.remove('ground');
+    ground.classList.add('groundStatic'); 
+  }
+  function empezarFondo(){
+    stars.classList.remove('starsStatic');
+    stars.classList.add('stars');
+    mountains.classList.remove('mountainsStatic');
+    mountains.classList.add('mountains');
+    ground.classList.remove('groundStatic');
+    ground.classList.add('ground'); 
+  }
+
+  let intervalId = setInterval(function () {
+    
+      if (space) {
+        cowboy.className = '';
+        cowboy.classList.add("cowboyJump");
+      } else if (keyDown) {
+        cowboy.className = '';
+        cowboy.classList.add('cowboyDown');
+        detenerFondo()
+      }else if(dead){
+        detenerFondo();
+        cowboy.classList.remove('cowboyDead');
+        cowboy.classList.add('cowboyStandDie');
+        clearInterval(intervalId);
+        ResetGame();
+      } 
+      else {
+        cowboy.className = '';
+        cowboy.classList.add('cowboyStand')
+      }
+    
+    
   }, 50)
 
+  function resetGame(){
+
+  }
 
   /* 
   K
