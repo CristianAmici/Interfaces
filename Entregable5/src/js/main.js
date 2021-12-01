718bff4841d4ded627e84a81150066b5eae1e12f
camilopez <camilopez8501@gmail.com>
2021-12-01 02:51:09 -0300

    sidebar, nav inpage

--- Entregable5/src/js/main.js
+++ Entregable5/src/js/main.js
@@ -1,11 +1,54 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function () {
 
+    let btinicio = document.getElementById("inicio");
+    btinicio.addEventListener("click", async () => {
+        document.getElementById("inicioPag").innerHTML = "Cargando...";
+        borrarBarra();
+        btinicio.classList.add("inPage");
+        /* setTimeout(function () ) */
+        try {
+            let response = await fetch("perfil.html");
+            if (response.ok) {
+                let html = await response.text();
+                let div = document.getElementById("inicioPag");
+                div.innerHTML = html;
 
+
+            }
+
+        } catch (error) {
+            document.getElementById("inicioPag").innerHTML = "error";
+        }
+    });
+   
+   
+    let btnotificaciones = document.getElementById("notificaciones");
+    btnotificaciones.addEventListener("click", async () => {
+        document.getElementById("inicioPag").innerHTML = "Cargando...";
+        borrarBarra();
+        btnotificaciones.classList.add("inPage");
+        /* setTimeout(function () ) */
+        try {
+            let response = await fetch("perfil.html");
+            if (response.ok) {
+                let html = await response.text();
+                let div = document.getElementById("inicioPag");
+                div.innerHTML = html;
+
+
+            }
+
+        } catch (error) {
+            document.getElementById("inicioPag").innerHTML = "error";
+        }
+    });
 
     let btperfil = document.getElementById("perfil");
     btperfil.addEventListener("click", async () => {
         document.getElementById("inicioPag").innerHTML = "Cargando...";
+        borrarBarra();
+        btperfil.classList.add("inPage");
         /* setTimeout(function () ) */
         try {
             let response = await fetch("perfil.html");
@@ -25,6 +68,8 @@ document.addEventListener("DOMContentLoaded", function () {
     let btmensajes = document.getElementById("mensajes");
     btmensajes.addEventListener("click", async () => {
         document.getElementById("inicioPag").innerHTML = "Cargando...";
+        borrarBarra();
+        btmensajes.classList.add("inPage");
         /* setTimeout(function () ) */
         try {
             let response = await fetch("mensajes.html");
@@ -42,7 +87,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
-
+    function borrarBarra(){
+        btinicio.className = '';
+        btinicio.classList.add("buttonMenu");
+        btperfil.className = '';
+        btperfil.classList.add("buttonMenu");
+        btmensajes.className = '';
+        btmensajes.classList.add("buttonMenu");
+        btnotificaciones.className = '';
+        btnotificaciones.classList.add("buttonMenu");
+    }
 
 
 
