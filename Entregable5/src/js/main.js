9e8bb699d91d0fe5f2536017867e7e2310a6a8ed
Cristian Amici <cristianamici@gmail.com>
2021-12-03 12:39:34 -0300

    boton publicar

--- Entregable5/src/js/main.js
+++ Entregable5/src/js/main.js
@@ -1,50 +1,10 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function () {
     
-    let btnpublicaciones = document.getElementById("publicaciones");
-    
-    let btninformacion = document.getElementById("informacion");
-    btninformacion.addEventListener("click", async () => {
-        document.getElementById("renderPerfil").innerHTML = "Cargando...";
-        borrarBarra();
-        btninformacion.classList.add("inPage");
-        try {
-            let response = await fetch("info.html");
-            if (response.ok) {
-                let html = await response.text();
-                let div = document.getElementById("renderPerfil");
-                div.innerHTML = html;
-            }
-
-        } catch (error) {
-            document.getElementById("renderPerfil").innerHTML = "error";
-        }
-    });
-
-    let btnfotos = document.getElementById("fotos");
-    btnfotos.addEventListener("click", async () => {
-        document.getElementById("renderPerfil").innerHTML = "Cargando...";
-        borrarBarra();
-        btnfotos.classList.add("inPage");
-        try {
-            let response = await fetch("fotos.html");
-            if (response.ok) {
-                let html = await response.text();
-                let div = document.getElementById("renderPerfil");
-                div.innerHTML = html;
-
-
-            }
-
-        } catch (error) {
-            document.getElementById("renderPerfil").innerHTML = "error";
-        }
-    });
-
     let btnbusqueda = document.getElementById("busqueda");
-    btnbusqueda.addEventListener('keyup', busqueda (e));
-    async function  busqueda(e){
-        if (e.key === 'Enter') {
+    btnbusqueda.addEventListener('', busqueda (Event));
+    async function  busqueda(Event){
+        if (Event.key === 'Enter') {
             document.getElementById("renderPerfil").innerHTML = "Cargando...";
             try {
                 let response = await fetch("busqueda.html");
@@ -61,9 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
     }
-
-    nuevaPublicacion
-
+    
     function borrarBarra() {
         btnpublicaciones.className = '';
         btnpublicaciones.classList.add("buttonMenu");
